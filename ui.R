097c65fbd4990046586f5187ec93a3f00ef7321b
Jason Chong <jasonchongg@gmail.com>
2017-10-13 02:59:42 -0700

    build of basic functionalities of shiny app. feature exploration, description, histogram

---
+++ ui.R
@@ -0,0 +1,40 @@
+library(shiny)
+
+# Define UI for miles per gallon application
+shinyUI(
+  
+  fluidPage(
+  # Application title
+  headerPanel("Spotify Top 200 Tracks Feature Explorer"),
+  
+  sidebarPanel(
+    sliderInput(inputId = "rank",
+                label = "Top Chart Ranks Included",
+                value = c(1,200),
+                min = 1,
+                max = 200),
+
+    selectInput(inputId = "feature", label = "Feature:",
+                choices = c("danceability", "energy", "key",
+                  "loudness", "mode", "speechiness", 
+                  "acousticness", "instrumentalness",
+                  "liveness", "valence", "tempo", "duration_ms",
+                  "time_signature")),
+    
+    sliderInput(inputId = "bins",
+                label = "Histogram Bins",
+                value = 25,
+                min = 1,
+                max = 50)
+    
+  ),
+  
+  mainPanel(
+    plotOutput("histogram"),
+    textOutput(outputId = "description")
+  
+  )
+  
+  
+  )
+)
